2332767ba7d289fea71f62b58fa5c6a06c251aa4
Erika Violet <erika.violet.osborn@gmail.com>
2023-03-02 17:15:45 +1100

    mayb working

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
+import base64
+import json
 import random
-import time
-import urllib
+import requests
+from google.auth.transport.requests import Request
+from google.oauth2 import id_token
+from google.auth import default
 
-import google.auth.transport.requests
-import google.oauth2.id_token
-
-
-def run_job(event, context):
-  data = base64.b64decode(event['data']).decode('utf-8')
-  print(data)
-
-  auth_req = google.auth.transport.requests.Request()
-  id_token = google.oauth2.id_token.fetch_id_token(auth_req, data['url'])
-
-  req = urllib.request.Request(data['url'])
-  req.add_header('Authorozation', f'Bearer {id_token}')
-
+# This function is triggered by a Pub/Sub message containing a JSON payload
+def process_message(event, context):
   state = random.randint(0,1)
 
   if state == 0:
-    print('mock success')
-    response = urllib.request.urlopen(req)
-    return 'ok', 200
+    print('Mock job success')
+    # Decode the Base64-encoded message payload
+    message_data = base64.b64decode(event['data']).decode('utf-8')
+    message = json.loads(message_data)
+    url = message['url']
+
+    # Generate an ID token using the default service account's credentials
+    request = Request()
+    token = id_token.fetch_id_token(request, url)
+
+    # Make the GET request to the specified URL using the generated ID token as the Authorization header
+    headers = {
+      'Authorization': f'Bearer {token}',
+      'Content-Type': 'application/json'
+    }
+    response = requests.get(url, headers=headers)
+
+    # Log the response
+    print(f"GET request to {url} returned {response.status_code}: {response.text}")
   else:
-    print('mock timeout')
-    return 'ok', 200
+    print('Mock job timeout')
