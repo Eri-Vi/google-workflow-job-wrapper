6b86e4f10794402dd6f1a7734d07b96af88306bf
Erika Violet <erika.violet.osborn@gmail.com>
2023-03-02 21:21:23 +1100

    mayb working now?

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import base64
 import json
 import random
 import requests
-from google.auth.transport.requests import Request
-from google.oauth2 import id_token
-from google.auth import default
+import google.auth.transport.requests
+
+credentials, _ = google.auth.default(scopes=['https://www.googleapis.com/auth/cloud-platform'])
+auth_req = google.auth.transport.requests.Request()
 
 # This function is triggered by a Pub/Sub message containing a JSON payload
 def process_message(event, context):
@@ -18,8 +19,8 @@ def process_message(event, context):
     url = message['url']
 
     # Generate an ID token using the default service account's credentials
-    request = Request()
-    token = id_token.fetch_id_token(request, url)
+    credentials.refresh(auth_req)
+    token = credentials.token
 
     # Make the GET request to the specified URL using the generated ID token as the Authorization header
     headers = {
